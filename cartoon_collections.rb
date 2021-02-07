2ae1b462c3e35cf5602860f6a6c11449838132f8
George Dilthey <george.dilthey@gmail.com>
2021-02-07 00:05:36 -0500

    commit tree 906553445601976979f5aacff358f5fc32d0c1bb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+    array.each_with_index do |i, index|
+        puts "#{index+1}. #{i}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+    array.map do |i|
+      i = i.capitalize
+      i << "!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+    array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|i| cheese_types.include?(i.to_s)}   
 end
+
+
+# dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"] 
+# roll_call_dwarves(dwarves)
+
+# planeter_calls = ["earth", "wind", "fire", "water", "heart"] 
+# puts summon_captain_planet(planeter_calls)
+
+# assorted_words = ["two", "go", "hi", "bop"]
+# puts long_planeteer_calls(assorted_words)
+
+snacks = ["crackers", "asdf", "thyme"]
+puts find_the_cheese(snacks)
